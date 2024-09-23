95a0201d337414f98e32386b7ab0b24973950269
Diogo <d.marti@ua.pt>
2024-09-23 19:43:48 +0100

    cpp09ex00_update

--- CPP 09/ex00/BitcoinExchange.cpp
+++ CPP 09/ex00/BitcoinExchange.cpp
@@ -56,3 +56,71 @@ void BitcoinExchange::readData(std::map<std::string, std::string>& map)
     }
     fstream.close();
 }
+
+static float getRate(std::map<std::string, std::string>& map, const std::string& date, const std::string& rate)
+{
+    for (std::map<std::string, std::string>::reverse_iterator it = map.rbegin();
+		 it != map.rend(); ++it)
+    {
+        if (it->first <= date)
+            return (convertString<float>(rate) * convertString<float>(it->second));
+    }
+    throw(BitcoinExchange::BitcoinExchangeException());
+}
+
+static void convertRates(const std::map<std::string, std::string>& map, const std::string& buffer)
+{
+    std::stringstream ssBuffer(buffer);
+    std::string DateRate[2];
+    float flo;
+
+    getline(ssBuffer, DateRates[0],'|');
+    while (DateRate[1][0] == ' ')
+        DateRate[1].erase(0,1);
+    if (DateRate[1].empty())
+    {
+        std::cerr << "Error: bad input => " << buffer << std::endl;
+        return ;
+    }
+    flo = convertString<float>(DateRate[1]);
+    if (validDate(DateRate[0]) && validRate(flo))
+    {
+        try
+        {
+            std::cout << std::fixed<< std::setprecision(2);
+            std::cout << DateRate[0] << " => " << flo << " = ";
+            std::cout << getRate(map, DateRate[0], DateRate[1]) << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what();
+            std::cerr << "Error: bad input => " << DateRate[0] << std::endl;
+        }
+        
+    }
+}
+
+void BitcoinExchange::exchangeBitcoin(const std::map<std::string,std::string>& map, const std::string& file)
+{
+    std::fstream fstream;
+    std::string buffer;
+
+    openfile(file, &fstream);
+    if (!fstream.is_open())
+        return ;
+    if (!getline(fstream, buffer) || buffer != "date | rate")
+    {
+        std::cout << "Error: " << file << ": empty or unsupported file\n";
+        fstream.close();
+        return ;
+    }
+    while (getline(fstream, buffer))
+        convertRates(map, buffer);
+    fstream.close();
+}
+
+const char* BitcoinExchange::BitcoinExchangeException::what() const throw()
+{
+    return ("Error: Date is too low.\n");
+}
+
